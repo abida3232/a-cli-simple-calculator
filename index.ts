fad7bd7a9ffa833435a30f505ac7552ec6183992
abida3232 <abidaasgher3232@gmail.com>
2024-06-14 19:09:54 +0500

    push my first calculator

---
+++ index.ts
@@ -0,0 +1,29 @@
+#!/usr/bin/env node
+//shabang
+import inquirer from "inquirer";
+
+let answers= await inquirer.prompt([
+    {message:"enter your first number",type:"number",name:"firstnumber"},
+    {message:"enter your second number",type:"number",name:"secondnumber"},
+    {
+        message:"select one operator to perform operation",
+        type:"list",
+        name:"operator",
+        choices:["addition", "subtraction","multiplication","division"]
+    },
+]);
+//if else condions apply
+
+if( answers.operator==="addition"){
+    console.log(answers.firstnumber + answers.secondnumber)}
+    else if(answers.operator==="subtraction"){
+        console.log(answers.firstnumber - answers.secondnumber)
+    }
+    else if( answers.operator==="multiplication"){
+        console.log(answers.firstnumber * answers.secondnumber)}
+        else if(answers.operator==="division"){
+            console.log(answers.firstnumber / answers.secondnumber)
+        }
+        else{
+            console.log("invalid input")
+        };
